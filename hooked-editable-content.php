0232acbebea843eaab75bcacf0431e454f8a3a18
Jon Anwyl <jon.anwyl@gmail.com>
2017-07-15 06:40:26 +0100

    Add in Freemius SDK

--- hooked-editable-content.php
+++ hooked-editable-content.php
@@ -18,6 +18,130 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+// Create a helper function for easy SDK access.
+function hec_fs() {
+	global $hec_fs;
+
+	if ( ! isset( $hec_fs ) ) {
+		// Include Freemius SDK.
+		require_once dirname(__FILE__) . '/freemius/start.php';
+
+		$hec_fs = fs_dynamic_init( array(
+			'id'                  => '1231',
+			'slug'                => 'hooked-editable-content',
+			'type'                => 'plugin',
+			'public_key'          => 'pk_3577291d27a65031a555c1992de85',
+			'is_premium'          => false,
+			'has_addons'          => false,
+			'has_paid_plans'      => false,
+			'menu'                => array(
+				'slug'           => 'edit.php?post_type=hec_hook',
+				'account'        => false,
+				'support'        => false,
+				'contact'        => false,
+			),
+		) );
+	}
+
+    return $hec_fs;
+}
+
+// Init Freemius.
+hec_fs();
+// Signal that SDK was initiated.
+do_action( 'hec_fs_loaded' );
+// Hook in uninstall actions.
+hec_fs()->add_action( 'after_uninstall', 'hec_fs_uninstall_cleanup' );
+
+/**
+ * Code to run on uninstall.
+ * @hooked after_uninstall
+ */
+function hec_fs_uninstall_cleanup() {
+
+	// Check capability.
+	if ( ! current_user_can( 'activate_plugins' ) ) {
+		return;
+	}
+
+	// Delete all hooks and specific content where appropriate.
+	$args = array(
+		'posts_per_page'	=>	-1,
+		'post_type'			=>	'hec_hook',
+		'post_status'		=>	array(
+									'publish',
+									'future',
+									'draft',
+									'pending',
+									'private',
+									'trash',
+									'auto-draft',
+									'inherit'
+								),
+	);
+
+	if ( $hooks = get_posts( $args ) ) {
+
+		// Work out whether we need to retain hooks with content.
+		$settings = get_option( 'hooked_editable_content_options' );
+		$retain_content = true;
+		if ( isset( $settings['content-delete'] ) ) {
+			if ( $settings['content-delete'] ) {
+				$retain_content = false;
+			}
+		}
+		
+		$args = array(
+			'posts_per_page'	=>	-1,
+			'post_status'		=>	array(
+										'publish',
+										'future',
+										'draft',
+										'pending',
+										'private',
+										'trash',
+										'auto-draft',
+										'inherit'
+									),
+			'post_type'			=>	'any'
+		);
+		
+		foreach ( $hooks as $hook ) {
+			
+			$remove = true;
+			if ( $retain_content ) {
+				if ( ! empty( $hook->post_content ) ) {
+					$remove = false;
+				} else {
+					
+					$args['meta_query'] = array(
+						array(
+							'key'			=>	'hec_content_' . $hook->ID,
+							'compare'		=>	'!=',
+							'value'			=>	''
+						)
+					);
+					
+					if ( $posts_with_content = get_posts( $args ) ) {
+						$remove = false;
+					}
+				}
+			}
+			
+			if ( $remove ) {
+				delete_post_meta_by_key( 'hec_content_' . $hook->ID );
+				wp_delete_post( $hook->ID, true );
+			}
+			
+		}
+		
+	}
+
+	// Delete options.
+	delete_option('hooked_editable_content_options');
+	
+}
+
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-hec-activator.php
